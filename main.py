26a03e01874eaecf8dd71085e1ca2174a626a191
Mohit7725 <62228085+Mohit7725@users.noreply.github.com>
2023-09-01 20:23:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Taking two inputs from the user
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+# 1. Floor Division
+floor_division_result = a // b
+print("Floor Division Result:", floor_division_result)
+
+# 2. a power b
+power_result = a ** b
+print("a power b Result:", power_result)
+
+# 3. Check if a and b are equal
+equal_check = a == b
+print("Are a and b equal?", equal_check)
